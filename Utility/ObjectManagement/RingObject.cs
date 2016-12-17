74c08797115efe070b42dd2011069287f46f90e9
Leo <2541619256@qq.com>
2016-12-17 11:13:49 +0800

    工具类修改名称空间

---
+++ Utility/ObjectManagement/RingObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ONetworkTalk.Utility.ObjectManagement
+{
+    public sealed class RingObject<T>
+    {
+        private T[] items;
+
+        private int currentPointerIndex = 0;
+
+        public RingObject(IEnumerable<T> col)
+        {
+            items = new T[col.Count()];
+            int tempIndex = 0;
+            foreach (var item in col)
+            {
+                items[tempIndex] = item;
+                tempIndex++;
+            }
+        }
+
+        public T GetNext()
+        {
+            if (items != null && items.Length == 0)
+            {
+                return default(T);
+            }
+            int i = Interlocked.Increment(ref currentPointerIndex);
+            return items[i % items.Length];
+        }
+    }
+}
