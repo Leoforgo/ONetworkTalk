8846ea6f18381e5ed9fbd52f24240fc93b7ced53
Leo <2541619256@qq.com>
2020-01-27 10:22:39 +0800

    实现websocket

---
+++ Websocket/WebSocketConnection.cs
@@ -0,0 +1,44 @@
+﻿using DotNetty.Codecs.Http.WebSockets;
+using DotNetty.Transport.Channels;
+using System;
+using System.Threading.Tasks;
+
+namespace ONetworkTalk.Websocket
+{
+    class WebSocketConnection : BaseTcpSocketConnection<IWebSocketServer, IWebSocketConnection, string>, IWebSocketConnection
+    {
+        #region 构造函数
+
+        public WebSocketConnection(IWebSocketServer server, IChannel channel, TcpSocketServerEvent<IWebSocketServer, IWebSocketConnection, string> serverEvent)
+            : base(server, channel, serverEvent)
+        {
+
+        }
+
+        #endregion
+
+        #region 私有成员
+
+        #endregion
+
+        #region 外部接口
+
+        public async Task Send(string msgStr)
+        {
+            try
+            {
+                await _channel.WriteAndFlushAsync(new TextWebSocketFrame(msgStr));
+                await Task.Run(() =>
+                {
+                    _serverEvent.OnSend?.Invoke(_server, this, msgStr);
+                });
+            }
+            catch (Exception ex)
+            {
+                _serverEvent.OnException?.Invoke(ex);
+            }
+        }
+
+        #endregion
+    }
+}
